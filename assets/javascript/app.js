3c8ecd13ed386ce0270493960efbd02889422b59
mchlltt <me@northwestern.edu>
2016-11-05 10:28:12 -0500

    Rearrange code based on class intro

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
 
-    // AJAX parameters
-    var queryMethod = 'GET';
-    var queryURL = 'http://pokeapi.co/api/v2/pokemon/';
-
+    // Variables
     // Global variable with current question content.
     var currentQuestion = {
         name: '',
@@ -18,10 +15,46 @@ $(document).ready(function() {
     var answersTimedOut = 0;
 
 
+    // On-click events
+    // When the start button is clicked.
+    $('#start-button').on('click', function() {
+        // Select a question.
+        game.selectQuestion();
+        // Call start game DOM functions.
+        DOMFunctions.callDOMFunctions(event = 'startGame');
+    });
+
+    // When an answer is clicked.
+    $(document).on('click', '.option', function() {
+        // Record name attribute from selected answer.
+        var answerText = $(this).data('name');
+        // Check if the answer is correct.
+        var answerCorrectness = game.isCorrect(answerText);
+        // Stop timer.
+        timer.stop();
+        // Update scores based on result.
+        game.updateScores(answerCorrectness);
+        // Call DOM question answered functions.
+        DOMFunctions.callDOMFunctions(event = 'questionAnswered', correct = answerCorrectness);
+    });
+
+    // When the start over button is clicked.
+    $('#restart-button').on('click', function() {
+        // Reset game variables.
+        game.resetGame();
+        // Reset DOM.
+        DOMFunctions.callDOMFunctions(event = 'restartClicked');
+    });
+
+
+    // Function objects.
     // Game logic object.
     var game = {
         // Variables
         questionsAsked: 0,
+        // AJAX parameters
+        queryMethod: 'GET',
+        queryURL: 'http://pokeapi.co/api/v2/pokemon/',
         // Methods
         selectQuestion: function() {
             // Verify that the game is not done.
@@ -34,18 +67,21 @@ $(document).ready(function() {
                 var idArray = [];
                 while (idArray.length < 4) {
                     var pokemonID = Math.ceil(Math.random() * 721);
-                    if (idArray.indexOf(pokemonID) !== -1) continue;
-                    idArray.push(pokemonID);
+                    if (idArray.indexOf(pokemonID) !== -1) {
+                        idArray.push(pokemonID);
+                    }
                 }
                 // Randomly select which of the 4 numbers will be the answer ID.
                 currentQuestion.answerIndex = Math.floor(Math.random() * 4);
                 // For each ID, push the name and id to the possible answers array.
                 idArray.forEach(function(element) {
                     // Query Pokeapi
-                    $.ajax(url = queryURL + element + '/', method = queryMethod)
+                    $.ajax(url = game.queryURL + element + '/', method = game.queryMethod)
                         .done(function(response) {
+                            // Push name and ID to currentQuestion.
                             currentQuestion.answers.push(response.species.name);
                             currentQuestion.ids.push(response.id);
+                            // Check whether to start the question.
                             game.startQuestion();
                         });
                 });
@@ -92,46 +128,26 @@ $(document).ready(function() {
         }
     };
 
-    // Timeout and interval object
-    var timer = {
-        // Count begins at 8.
-        count: 9,
-        start: function() {
-            timer.decrement();
-            counter = setInterval(timer.decrement, 1000);
-        },
-        // Decrease time by one per second. If the time hits 0, stop timer.
-        decrement: function() {
-            timer.count--;
-            DOMFunctions.callDOMFunctions(event = 'countDown', correct = null, timeRemaining = timer.count);
-            if (timer.count === 0) {
-                timer.stop();
-                DOMFunctions.callDOMFunctions(event = 'questionAnswered', correct = '');
-                game.updateScores();
-            }
-        },
-        // Clear interval and reset count.
-        stop: function() {
-            clearInterval(counter);
-            timer.count = 9;
-        },
-    };
-
 
     // DOM writing object
     var DOMFunctions = {
         displayTimer: function() {
+            // Display timer div.
             $('.timer').css('display', 'block');
         },
         displayTimeLeft: function(timeRemaining) {
+            // Update time in timer.
             $('#timer').text(timeRemaining);
         },
         displayLoadingGIF: function() {
+            // Hide start button.
+            $('.start').hide();
+            // Show timer div and loading image.
             $('.timer').css('display', 'block');
             $('.loading-image').css('display', 'block');
         },
         displayQuestion: function() {
-            // Hide loading image, show question
+            // Hide loading image, show question.
             $('.loading-image').hide();
             $('.question-display').show();
             // Show answer options, empty any previous options.
@@ -153,8 +169,10 @@ $(document).ready(function() {
             }
         },
         displayAnswer: function(correct) {
+            // Hide timer and answer options
             $('.timer').hide();
             $('.answer-display').hide();
+            // Show results and correct answer.
             $('.question-result').show();
             $('#correct-answer').text('It\'s ' + currentQuestion.answers[currentQuestion.answerIndex] + '!');
             // Remove silhouette class.
@@ -173,36 +191,41 @@ $(document).ready(function() {
             setTimeout(game.selectQuestion, 1500);
         },
         hideAnswer: function() {
-            // Hide answer section.
+            // Hide answer section and show the timer again.
             $('.timer').show();
             $('.question-result').hide();
         },
         displayResults: function() {
+            // Hide question and timer.
             $('.question-display').hide();
             $('.timer').hide();
+            // Show results and restart button.
             $('.game-results').show();
+            $('.restart').show();
             // Display answer counts.
             $('#questions-correct').text('Correct: ' + answersCorrect);
             $('#questions-incorrect').text('Incorrect: ' + answersIncorrect);
             $('#questions-unanswered').text('Unanswered: ' + answersTimedOut);
-            $('.restart').show();
         },
         displayNewGame: function() {
+            // Hide restart, timer section, and game results.
             $('.restart').hide();
-            $('#timer').text('');
             $('.timer').hide();
-            $('.question-image').addClass('silhouette');
             $('.game-results').hide();
+            // Clear timer text.
+            $('#timer').text('');
+            // Show start button.
             $('.start').show();
+            // Ensure the first image will be silhouetted
+            $('.question-image').addClass('silhouette');
         },
         callDOMFunctions: function(event, correct, timeRemaining) {
             // Pipe calls from outside the object to the correct function.
             if (event === 'startGame') {
-                $('.start').hide();
                 DOMFunctions.displayLoadingGIF();
             } else if (event === 'newQuestion') {
-                DOMFunctions.displayQuestion();
                 DOMFunctions.hideAnswer();
+                DOMFunctions.displayQuestion();
                 DOMFunctions.displayTimer();
             } else if (event === 'countDown') {
                 DOMFunctions.displayTimeLeft(timeRemaining);
@@ -217,35 +240,34 @@ $(document).ready(function() {
         }
     };
 
-    // On-click events
-    // When the start button is clicked.
-    $('#start-button').on('click', function() {
-        // Call start game DOM functions.
-        DOMFunctions.callDOMFunctions(event = 'startGame');
-        // Select a question.
-        game.selectQuestion();
-    });
+    // Timeout and interval object
+    var timer = {
 
-    // When an answer is clicked.
-    $(document).on('click', '.option', function() {
-        // Record name attribute from selected answer.
-        var answerText = $(this).data('name');
-        // Check if the answer is correct.
-        var answerCorrectness = game.isCorrect(answerText);
-        // Stop timer.
-        timer.stop();
-        // Call DOM question answered functions.
-        DOMFunctions.callDOMFunctions(event = 'questionAnswered', correct = answerCorrectness);
-        // Update scores based on result.
-        game.updateScores(answerCorrectness);
-    });
+        // Count variable begins at 8.
+        count: 9,
 
-    // When the start over button is clicked.
-    $('#restart-button').on('click', function() {
-        // Reset game variables.
-        game.resetGame();
-        // Reset DOM.
-        DOMFunctions.callDOMFunctions(event = 'restartClicked');
-    });
+        // Start function begins countdown.
+        start: function() {
+            timer.decrement();
+            counter = setInterval(timer.decrement, 1000);
+        },
+
+        // Decrease time by one per second. If the time hits 0, stop timer.
+        decrement: function() {
+            timer.count--;
+            DOMFunctions.callDOMFunctions(event = 'countDown', correct = null, timeRemaining = timer.count);
+            if (timer.count === 0) {
+                timer.stop();
+                game.updateScores();
+                DOMFunctions.callDOMFunctions(event = 'questionAnswered', correct = '');
+            }
+        },
+
+        // Clear interval and reset count.
+        stop: function() {
+            clearInterval(counter);
+            timer.count = 9;
+        },
+    };
 
 });
